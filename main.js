636e64c80c9e11a41ac999bba860bb1e93c8878a
Pierre Nogaro <pierre.nogaro@gmail.com>
2023-10-19 17:13:37 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// Cours recap javaScript :
+
+// 1. Les variables :
+// let & const - Conteneur qui permet de stocker des informations.
+
+// 2. console.log :
+// nous permet d'afficher dans la console.
+
+// 3. Concaténation :
+let prenom = "Pierre"
+let nom = "Nogaro"
+let mot = "Bonjour"
+
+
+
+
+
+
